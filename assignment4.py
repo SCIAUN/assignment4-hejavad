4139db22b8307c6494f66403fe19dd37c8029730
hejavad <he.javad@yahoo.com>
2018-04-27 13:41:24 +0430

    s

--- assignment4.py
+++ assignment4.py
@@ -1,4 +1,21 @@
-def port_scanner():
+
+import nmap
+
+def port_scanner(ports):
+    nm = nmap.PortScanner()
+    print(nm.scan(ports, '21-1000'))
+    nm.scan(ports, '21-1000')
+    print(ports)
+    print(nm.all_hosts())
+    print(ports)
+    for host_names in nm.all_hosts():
+        print(ports)
+        ports = nm[host_names].get('tcp')
+        print(ports)
+        write_to_file(ports)
+
+    return ports
+
     """
     write a port scanner using nmap library
     for scanning 10 arbitrary hosts and and ports between
@@ -7,16 +24,30 @@ def port_scanner():
     """
 
 def write_to_file(contents):
+    f = open('result_of_nmap', 'w')
+
+    f.write(contents)
     """
     first remove the pass from function body
     then write your code
     :param contents: the result that should be written to file
     :return: true if the writing successfull and false if can't
     """
+
     pass
+
+def get_host_names():
+    f = open('host_names', 'r')
+    hosts = f.read()
+    hosts = hosts.rsplit('\n')
+    return hosts
+
+
 
 
 def main():
-    port_scanner()
+    hosts = get_host_names()
+    for host_name in hosts:
+        port_scanner(host_name)
 
 main()
